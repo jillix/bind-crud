bdc153dfe75f228a2b357f1e7b622bba47cdbfe6
Ionica Bizau <bizauionica@yahoo.com>
2013-07-25 18:36:18 +0300

    Hardcoded the response of server.

--- operations.js
+++ operations.js
@@ -2,6 +2,59 @@ var model = require('./model');
 var templates = require('./templates');
 
 exports.find = function (link) {
+    if (link.data.t === "_list") {
+        link.send(200, [
+            {
+                    "_id" : "51f13029f77a09097eac5f19",
+                    "name" : "Folder A",
+                    "type" : "folder",
+                    "_ln" : [
+                            {
+                                    "_tp" : "_template",
+                                    "_id" : "user"
+                            }
+                    ]
+            },
+            {
+                    "_id" : "51f13031f77a09097eac5f1a",
+                    "name" : "Folder B",
+                    "type" : "folder",
+                    "_ln" : [
+                            {
+                                    "_tp" : "_template",
+                                    "_id" : "user"
+                            }
+                    ]
+            },
+            {
+                    "_id" : "51f13037f77a09097eac5f1b",
+                    "name" : "Folder C",
+                    "type" : "folder",
+                    "_ln" : [
+                            {
+                                    "_tp" : "_template",
+                                    "_id" : "user"
+                            }
+                    ]
+            },
+            {
+                    "_id" : "51f13087f77a09097eac5f1c",
+                    "name" : "Filtered 1",
+                    "type" : "filtered",
+                    "_ln" : [
+                            {
+                                    "_tp" : "_template",
+                                    "_id" : "user"
+                            },
+                            {
+                                    "_tp" : "_list",
+                                    "_id" : "51f13031f77a09097eac5f1a"
+                            }
+                    ]
+            }
+        ]);
+        return;
+    }
     model('find', link);
 };
 
