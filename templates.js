86e859253860a413805842741b2ebc59ecc6a2b1
Adrian Ottiker <adrian@ottiker.com>
2013-08-01 14:27:01 +0200

    get also all templates with getTemplates operation

--- templates.js
+++ templates.js
@@ -86,151 +86,163 @@ function initAndCache (template) {
     return templateCache[template._id] = template;
 }
 
+function getCachedTemplates (templates, role) {
+    
+    if (!templates || templates.length === 0) {
+        return;
+    }
+    
+    var resultTemplates = {};
+    var queryTemplates = [];
+    var addedTemplates = {};
+    
+    if (templates && templates.length > 0) {
+        // check cached templates
+        for (var i = 0, l = templates.length; i < l; ++i) {
+            // add to result if role has access
+            if (templateCache[templates[i]] && checkAccess(templateCache[templates[i]], role, 1)) {
+                resultTemplates[templates[i]] = templateCache[templates[i]];
+            } else if (!addedTemplates[templates[i]]) {
+                addedTemplates[templates[i]] = true;
+                queryTemplates.push(templates[i]);
+            }
+        }
+    }
+    
+    return {
+        query: queryTemplates,
+        cached: resultTemplates
+    }
+}
+
 // TODO check access
 function fetchTemplatesFromDb (templates, role, fields, callback) {
+    
+    var tpls = getCachedTemplates(templates, role);
+    var resultTemplates = {};
     
     // build query
     var dbReq = {
         query: {
-            _id: templates.length > 1 ? {$in: []} : '',
             _tp: config.templateId
             //_ln: [{_tp: 'role'}]
         },
-        options: {limit: templates.length, fields: fields || {}},
+        options: {fields: fields || {}},
         template: templateCache.template
     };
     
-    // check if role has write access
-    //dbReq.query._ln[0]._id = {$gt: 0};
-    
-    if (templates.length > 1) {
-        for (var i = 0, l = templates.length; i < l; ++i) {
-            if (typeof templates[i] !== 'string' || templates[i].length < 1) {
-                var err = new Error('Invalid templates.');
-                err.statusCode = 400;
-                return callback(err);
-            }
+    if (tpls) {
+        var templates = tpls.query;
+        resultTemplates = tpls.cached;
+        
+        // check if role has write access
+        //dbReq.query._ln[0]._id = {$gt: 0};
+        
+        if (templates.length > 1) {
             
-            dbReq.query._id.$in.push(templates[i]);
+            dbReq.query._id ={$in: []};
+            dbReq.options.limit = templates.length;
+            
+            for (var i = 0, l = templates.length; i < l; ++i) {
+                if (typeof templates[i] !== 'string' || templates[i].length < 1) {
+                    var err = new Error('Invalid templates.');
+                    err.statusCode = 400;
+                    return callback(err);
+                }
+                
+                dbReq.query._id.$in.push(templates[i]);
+            }
+        } else if (templates.length > 0) {
+            
+            dbReq.options.limit = 1;
+            dbReq.query._id = templates[0];
         }
-    } else {
-        dbReq.query._id = templates[0];
     }
     
-    io.find(null, dbReq, function (err, cursor) {
-        
-        if (err) {
-            err.statusCode = 500;
-            return callback(err);
-        }
-        
-        if (!cursor) {
-            var err = new Error('Templates not found.');
-            err.statusCode = 404;
-            return callback(err);
-        }
-        
-        cursor.toArray(function (err, templates) {
-                
+    if (!tpls || tpls.query.length > 0) {
+        io.find(null, dbReq, function (err, cursor) {
+            
             if (err) {
                 err.statusCode = 500;
                 return callback(err);
             }
             
-            if (!templates || templates.length === 0) {
+            if (!cursor) {
                 var err = new Error('Templates not found.');
                 err.statusCode = 404;
                 return callback(err);
             }
             
-            callback(null, templates);
+            cursor.toArray(function (err, templates) {
+                    
+                if (err) {
+                    err.statusCode = 500;
+                    return callback(err);
+                }
+                
+                if (!templates || templates.length === 0) {
+                    var err = new Error('Templates not found.');
+                    err.statusCode = 404;
+                    return callback(err);
+                }
+                
+                for (var i = 0, l = templates.length; i < l; ++i) {
+                    var temp = initAndCache(templates[i]);
+                    if (temp) {
+                        resultTemplates[templates[i]._id] = temp;
+                    }
+                }
+                
+                callback(null, resultTemplates);
+            });
         });
-    });
+    } else {
+        callback(null, tpls.cached);
+    }
 }
 
 function getTemplate (request, callback) {
 
-    if (!templateCache[request.templateId]) {
-        return fetchTemplatesFromDb([request.templateId], request.role, {}, function (err, template) {
-            
-            if (err) {
-                err.statusCode = err.statusCode || 500;
-                return callback(err);
-            }
-            
-            request.template = initAndCache(template[0]);
-            
-            if (!request.template) {
-                err = new Error('Bad template object.');
-                err.statusCode = 500;
-                return callback(err);
-            }
-            
-            callback(null, request);
-        });
-    }
-    
-    // check access
-    if (!checkAccess(templateCache[request.templateId], request.role, 1)) {
-        var err = new Error('Templates not found.');
-        err.statusCode = 404;
-        return callback(err);
-    }
-    
-    request.template = templateCache[request.templateId];
-    callback(null, request);
+    fetchTemplatesFromDb([request.templateId], request.role, {}, function (err, template) {
+        
+        if (err) {
+            err.statusCode = err.statusCode || 500;
+            return callback(err);
+        }
+        
+        request.template = template[request.templateId];
+        
+        if (!request.template) {
+            err = new Error('Bad template object.');
+            err.statusCode = 500;
+            return callback(err);
+        }
+        
+        callback(null, request);
+    });
 }
 
 function getTemplates (link) {
     
     var templates = link.data;
-    var resultTemplates = {};
-    var queryTemplates = [];
-    var addedTemplates = {};
     
-    // check templates
-    if (!templates || templates.length < 1) {
-        return link.send(400, 'No templates given.');
-    }
-    
-    // check cached templates
-    for (var i = 0, l = templates.length; i < l; ++i) {
-        // add to result if role has access
-        if (templateCache[templates[i]] && checkAccess(templateCache[templates[i]], link.session._rid, 1)) {
-            resultTemplates[templates[i]] = {
-                id: templateCache[templates[i]]._id,
-                name: templateCache[templates[i]].name,
-                schema: templateCache[templates[i]].schema.paths
-            };
-        } else if (!addedTemplates[templates[i]]) {
-            addedTemplates[templates[i]] = true;
-            queryTemplates.push(templates[i]);
+    fetchTemplatesFromDb(templates, link.session._rid, {}, function (err, templates) {
+        
+        if (err) {
+            return link.send(err.statusCode || 500, err.message);
         }
-    }
-    
-    if (queryTemplates.length > 0) {
-        return fetchTemplatesFromDb(queryTemplates, link.session._rid, {}, function (err, templates) {
-            
-            if (err) {
-                return link.send(err.statusCode || 500, err.message);
-            }
-            
-            for (var i = 0, l = templates.length; i < l; ++i) {
-                resultTemplates[templates[i]._id] = initAndCache(templates[i]);
-                if (resultTemplates[templates[i]._id]) {
-                    resultTemplates[templates[i]._id] = {
-                        id: templateCache[templates[i]._id]._id,
-                        name: templateCache[templates[i]._id].name,
-                        schema: templateCache[templates[i]._id].schema.paths
-                    }
-                }
-            }
-            
-            link.send(200, resultTemplates);
-        });
-    }
-    
-    link.send(200, resultTemplates);
+        
+        var result = {};
+        for (var template in templates) {
+            result[template] = {
+                id: templates[template]._id,
+                name: templates[template].name,
+                schema: templates[template].schema.paths
+            };
+        }
+        
+        link.send(200, result);
+    });
 }
 
 exports.getTemplate = getTemplate;
