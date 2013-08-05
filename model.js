87efefbfdb0a44cfff06bb5703e793d279dad57a
Ionica Bizau <bizauionica@yahoo.com>
2013-08-05 18:40:43 +0300

    Convert elemMatch keys into objectid.

--- model.js
+++ model.js
@@ -3,14 +3,14 @@ var templates = require('./templates');
 var ObjectId = require('modm').ObjectId;
 
 function createRequest (method, link) {
-    
+
     var data = link.data || {};
-    
+
     // template id is mandatory
     if (!data.t) {
         return;
     }
-    
+
     var request = {
         role: link.session._rid,
         options: {},
@@ -20,18 +20,18 @@ function createRequest (method, link) {
     // query
     request.query = data.q || {};
     request.query._tp = data.t;
-    
+
     // update
     if (data.d && data.d.constructor.name === 'Object') {
-        
+
         // set type
         if (method === 'insert') {
             data.d._tp = data.t;
         }
-        
+
         request.data = data.d;
     }
-    
+
     // options
     if (data.o && data.o.constructor.name === 'Object') {
         request.options = data.o;
@@ -41,7 +41,7 @@ function createRequest (method, link) {
     if (data.f && data.f.constructor.name === 'Object') {
         request.options.fields = data.f;
     }
-    
+
     return request;
 }
 
@@ -49,7 +49,7 @@ function convertToObjectId (request) {
     try {
         // convert _id to MongoDB's ObjectId
         if (request.query._id) {
-            
+
             // convert with operators
             if (typeof request.query._id === 'object') {
                 for (var op in request.query._id) {
@@ -61,22 +61,35 @@ function convertToObjectId (request) {
                 request.query._id = ObjectId(request.query._id);
             }
         }
-        
+
         // convert _tp to MongoDB's ObjectId
         if (request.query['_tp']) {
             request.query['_tp'] = ObjectId(request.query['_tp']);
         }
-        
+
         // convert _ln._id to MongoDB's ObjectId
         if (request.query['_ln._id']) {
             request.query['_ln._id'] = ObjectId(request.query['_ln._id']);
         }
-        
+
         // convert _ln._tp to MongoDB's ObjectId
         if (request.query['_ln._tp']) {
-            request.query['_ln._tp'] = ObjectId(request.query['_ln._id']);
+            var tp = request.query['_ln._tp'];
+            for (var i in tp) {
+                tp[i] = ObjectId(tp[i]);
+            }
         }
-        
+
+        // convert _ln.$elemMatch keys to MongoDB's ObjectId
+        if (request.query['_ln']['$elemMatch']) {
+            var elemMatch = request.query['_ln']['$elemMatch'];
+            for (var i in elemMatch) {
+                if (['_id', '_tp'].indexOf(i) > -1) {
+                    elemMatch[i] = ObjectId(elemMatch[i]);
+                }
+            }
+        }
+
         return request;
     } catch (err) {
         return;
@@ -84,31 +97,31 @@ function convertToObjectId (request) {
 }
 
 module.exports = function (method, link) {
-    
+
     if (!io[method]) {
         return link.send(501, 'Method not implemented');
     }
-    
+
     // check parameters
     var request = createRequest(method, link);
 
     if (!request) {
         return link.send(400, 'Bad request.');
     }
-    
+
     // get template (cache)
     templates.getTemplate(request, function (err, request) {
-        
+
         if (err) {
             return link.send(err.statusCode || 500, err.message);
         }
-        
+
         request = convertToObjectId(request);
-        
+
         if (!request) {
             return link.send(400, 'Incorrect ObjectId format');
         }
-        
+
         // do input/output
         io[method](link, request);
     });
