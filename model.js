jillix/bind-crud

commit 4b0a2629e38d56879e514fdc851d2a40b1010be9

Moved addToTemplate to options key from template.

model.js
@@ -280,8 +280,8 @@ module.exports = function (method, link) {
             }
             
             // TODO This is a hack until we can merge the templates
-            if (request.template && request.template.addToTemplates && request.data && request.data._tp) {
-                var copy = request.template.addToTemplates.slice();
+            if (request.template && (request.template.options || {}).addToTemplates && request.data && request.data._tp) {
++               var copy = request.template.options.addToTemplates.slice();
                 copy.push(request.data._tp);
                 request.data._tp = copy;
             }

commit b2e385923dfa903d43ce9bd6bd9ebc394d1c6f54

Fixed syntax error introduced in 4b0a262

model.js
@@ -276,7 +276,7 @@ module.exports = function (method, link) {
             
             // TODO This is a hack until we can merge the templates
             if (request.template && (request.template.options || {}).addToTemplates && request.data && request.data._tp) {
-+               var copy = request.template.options.addToTemplates.slice();
+                var copy = request.template.options.addToTemplates.slice();
                 copy.push(request.data._tp);
                 request.data._tp = copy;
             }